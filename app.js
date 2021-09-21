61bb8d14c38dcd68d25c93ea78f6d2b613330579
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2021-09-22 04:59:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
- // Your web app's Firebase configuration
- // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
